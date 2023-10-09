fbadd76898c48b27688e3f4e3cb7e451e8ac53a6
Chinmay Shetye <chinmay.shetye3@gmail.com>
2023-10-09 13:23:31 +0530

    First commit of PyResearch project

---
+++ README.md
@@ -0,0 +1,4 @@
+# This is README file for PyResearch project
+# Author: Chinmay Shetye
+
+This project creates a pipeline which builds, tests and deploys a python project.
---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello world!")
+print("** Welcome to my python project PyResearch ***")
+print("--------------------This project is integrated with automated Jenkins pipeline------------------------")
+#code here
